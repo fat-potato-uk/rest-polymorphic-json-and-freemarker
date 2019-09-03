be6e2ec93f4e319fdb25073de7ccdf5b47ed7f8e
andrew <andrew@fat-potato.co.uk>
2019-09-03 14:05:20 +0100

    Fixed unneeded code

--- src/main/java/demo/models/Cat.java
+++ src/main/java/demo/models/Cat.java
@@ -1,10 +1,8 @@
 package demo.models;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Data;
 
 @Data
-@JsonTypeName("dog")
 public class Cat extends Animal{
     private int lives;
 }
